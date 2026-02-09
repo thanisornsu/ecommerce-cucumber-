9ed5c73b43cecf150484d2514a55bad7dd1a6763
thanisornsu <thanisorn.su@gmail.com>
2026-02-09 19:26:02 +0700

    test: increase cucumber step timeout for CI

--- cucumber.js
+++ cucumber.js
@@ -1,13 +1,14 @@
 module.exports = {
-    default: {
-        format: [
-            "progress-bar",
-            "json:reports/cucumber_report.json",
-            "html:reports/cucumber_report.html"
-        ],
-        requireModule: ["ts-node/register"],
-        require: ["src/steps/**/*.ts", "src/support/**/*.ts"],
-        paths: ["features/**/*.feature"],
-        publishQuiet: true
-    }
+  default: {
+    format: [
+      "progress-bar",
+      "json:reports/cucumber_report.json",
+      "html:reports/cucumber_report.html",
+    ],
+    requireModule: ["ts-node/register"],
+    require: ["src/steps/**/*.ts", "src/support/**/*.ts"],
+    paths: ["features/**/*.feature"],
+    publishQuiet: true,
+    timeout: 60000,
+  },
 };
